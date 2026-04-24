322181e571f3534331c1b6be665cb0e25d6af74e
Sergio Gabriel Rueda Piñón (Gapuccino) <gabrielpin2121@gmail.com>
2026-04-24 12:51:03 -0600

    fix: move non-blocking css to client component to fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import ExternalStyles from "@/components/ExternalStyles";
 
 const montserrat = Montserrat({
   subsets: ["latin", "latin-ext"],
@@ -31,15 +32,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" />
         <link rel="preload" href="/logo.webp" as="image" fetchPriority="high" />
-        <link 
-          rel="stylesheet" 
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
-          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
-          crossOrigin="anonymous" 
-          referrerPolicy="no-referrer"
-          media="print"
-          onLoad={(e: any) => { e.target.media = 'all' }}
-        />
+        <ExternalStyles />
       </head>
       <body>
         {children}
